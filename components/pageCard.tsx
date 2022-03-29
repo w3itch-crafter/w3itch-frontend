4ca7b02ac4b4ad5b5620b3bfe99bd258c8e404eb
Garfield550 <openpgp@550.moe>
2022-03-29 14:17:50 +0000

    feat: change style

--- components/pageCard.tsx
+++ components/pageCard.tsx
@@ -10,6 +10,9 @@ export default function PageCard({ children }: PageCardProps) {
     border: 1px solid #d0d0d0;
     box-shadow: 0 2px 4px rgb(0 0 0 / 5%);
     border-radius: 3px;
+    width: auto;
+    max-width: 960px;
+    margin: 20px auto;
   `
 
   return <Container>{children}</Container>
