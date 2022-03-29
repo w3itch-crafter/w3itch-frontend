dcf0725c31da8fa2239bf68589ab4512fe50cd8e
xiaotiandada <istianlei@qq.com>
2022-03-29 16:19:58 +0800

    fix: fix format

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,4 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled'
 import Link from 'next/link'
 
 export default function Footer() {
@@ -26,35 +26,35 @@ export default function Footer() {
 }
 
 const Container = styled.footer`
-margin: 20px 0 40px 0;
-padding: 0 20px;
-font-size: 14px;
-text-align: center;
-color: #858585;
+  margin: 20px 0 40px 0;
+  padding: 0 20px;
+  font-size: 14px;
+  text-align: center;
+  color: #858585;
 `
 
 const Primary = styled.div`
-line-height: 32px;
+  line-height: 32px;
 `
 
 const PrimaryLink = styled.a`
-padding: 0 15px;
-text-transform: uppercase;
-font-weight: bold;
-border: none;
-vertical-align: top;
-text-decoration: none;
-display: inline-block;
-color: inherit;
+  padding: 0 15px;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  vertical-align: top;
+  text-decoration: none;
+  display: inline-block;
+  color: inherit;
 `
 
 const Secondary = styled.div`
-margin-top: 10px;
-line-height: 25px;
+  margin-top: 10px;
+  line-height: 25px;
 `
 
 const Copyright = styled.span``
 
-const Spacer = styled.span`
-margin: 0 10px;
-`
+// const Spacer = styled.span`
+//   margin: 0 10px;
+// `
