8a89b372e7028adfc0180f68c5ae7f90bcf9533c
Garfield550 <openpgp@550.moe>
2022-04-06 11:54:23 +0000

    feat: add user profile home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,24 @@
 import assert from 'assert'
-import type { GetServerSideProps, NextPage } from 'next'
+import type { GetServerSideProps } from 'next'
+import { Fragment } from 'react'
+import { NextPageWithLayout } from 'types'
 import { URL } from 'url'
 
 import Home from './_home'
+import ProfileHome from './profile/_home'
 
 declare interface IndexProps {
   wildcard: string | null
 }
 
-const Index: NextPage<IndexProps> = (props) => {
+const Index: NextPageWithLayout<IndexProps> = (props) => {
   const { wildcard } = props
-  if (wildcard) return <div>{wildcard}</div>
+  if (wildcard) return <ProfileHome {...props} />
   return <Home {...props} />
+}
+
+Index.getLayout = function getLayout(page: React.ReactElement) {
+  return <Fragment>{page}</Fragment>
 }
 
 export const getServerSideProps: GetServerSideProps<IndexProps> = async (
@@ -21,7 +28,7 @@ export const getServerSideProps: GetServerSideProps<IndexProps> = async (
   assert(NEXT_PUBLIC_URL, new TypeError('No NEXT_PUBLIC_URL env.'))
   const publicUrl = new URL(NEXT_PUBLIC_URL)
   const { host } = context.req.headers
-  const wildcardTestRegex = new RegExp(`.${publicUrl.host}\\S+`)
+  const wildcardTestRegex = new RegExp(`.${publicUrl.host}.*`)
   const hasWildcard = host && wildcardTestRegex.test(host)
   const wildcard = hasWildcard ? host.replace(wildcardTestRegex, '') : null
   if (wildcard === null)
