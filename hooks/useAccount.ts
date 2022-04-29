37627ab98e08d54baaac54e1dc6f8e10932bd9b3
Garfield Lee <openpgp@550.moe>
2022-04-29 20:26:38 +0800

    feat: push to login when get user info failed

--- hooks/useAccount.ts
+++ hooks/useAccount.ts
@@ -1,21 +1,25 @@
-import { getMine, refresh } from 'api/account'
+import { getMe, getMine } from 'api'
 import { AuthenticationContext } from 'context'
-import { useCallback, useContext, useEffect, useMemo } from 'react'
+import { useRouter } from 'next/router'
+import { useCallback, useContext, useEffect } from 'react'
 
-export function useRefresh() {
+export function useAuthentication() {
+  const router = useRouter()
   const { state, dispatch } = useContext(AuthenticationContext)
-  const memoState = useMemo(() => state, [state])
   const fetchUser = useCallback(async () => {
     if (!state.isAuthenticated && !state.isLogout) {
-      const user = await refresh()
+      const user = await getMe()
       const account = await getMine()
+      if (user === null && account === null) {
+        return router.push('/login')
+      }
       dispatch({ type: 'LOGIN', payload: { user, account } })
     }
-  }, [dispatch, state.isAuthenticated, state.isLogout])
+  }, [dispatch, router, state.isAuthenticated, state.isLogout])
 
   useEffect(() => {
     fetchUser()
   }, [fetchUser])
 
-  return memoState
+  return state
 }
