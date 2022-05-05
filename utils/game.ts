36e5d9630518db6575f1a6357ca0fa3bd8c7a6b1
_XiaoTian <istianlei@qq.com>
2022-05-05 22:12:53 +0800

    feat: form appStoreLinks support string array

--- utils/game.ts
+++ utils/game.ts
@@ -1,6 +1,24 @@
+import { fromUrl, parseDomain } from 'parse-domain'
+import { toUnicode } from 'punycode'
+
 /**
  * calc rating
  * @param rating 100 - 500
  * @returns 1 - 5
  */
 export const calcRating = (rating: number) => parseInt(String(rating / 100))
+
+/**
+ * link domain parse
+ * @param link
+ * @returns
+ */
+export const linkDomainParser = (link: string): string => {
+  try {
+    const { domain } = parseDomain(fromUrl(link))
+    return toUnicode(domain)
+  } catch (error) {
+    console.error(error)
+    return 'Links'
+  }
+}
