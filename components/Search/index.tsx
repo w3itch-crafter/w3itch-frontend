a7923e56d3295113f1244844c7d43549b7963a3b
_XiaoTian <istianlei@qq.com>
2022-05-17 18:07:21 +0800

    feat: search component

--- components/Search/index.tsx
+++ components/Search/index.tsx
@@ -2,13 +2,8 @@ import '@algolia/autocomplete-theme-classic'
 
 import { autocomplete } from '@algolia/autocomplete-js'
 import { createQuerySuggestionsPlugin } from '@algolia/autocomplete-plugin-query-suggestions'
-import algoliasearch from 'algoliasearch/lite'
 import { useEffect, useRef } from 'react'
-
-const searchClient = algoliasearch(
-  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string,
-  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY as string
-)
+import { searchClient } from 'utils'
 
 const querySuggestionsPlugin = createQuerySuggestionsPlugin({
   searchClient,
@@ -27,15 +22,73 @@ const querySuggestionsPlugin = createQuerySuggestionsPlugin({
       templates: {
         ...source.templates,
         item(params) {
-          const { item, html } = params
-
+          const { item, html, components } = params
           return html`<a
-            class="aa-ItemLink"
+            class="custom-aa-ItemLink"
             href="${process.env.NEXT_PUBLIC_URL}/game/${item.id}"
             target="_blank"
           >
-            ${item.title}
+            <div class="InterfaceDemoHit aa-ItemWrapper">
+              <div class="aa-ItemContent">
+                <div class="aa-ItemIcon">
+                  <img
+                    src="${item.cover}"
+                    alt="${item.title}"
+                    width="40"
+                    height="40"
+                  />
+                </div>
+                <div class="aa-ItemContentBody">
+                  <div
+                    class="aa-ItemContentTitle custom-aa-ItemContentTitle PrimaryAttribute"
+                  >
+                    ${components.Highlight({
+                      hit: item,
+                      attribute: 'title',
+                      tagName: 'span',
+                    })}
+                  </div>
+                  <div
+                    class="aa-ItemContentDescription custom-aa-ItemContentDescription SecondaryAttribute"
+                  >
+                    ${components.Snippet({
+                      hit: item,
+                      attribute: 'subtitle',
+                    })}
+                  </div>
+                  <div
+                    class="aa-ItemContentDescription custom-aa-ItemContentDescription ThirdAttribute"
+                  >
+                    ${components.Snippet({
+                      hit: item,
+                      attribute: 'username',
+                    })}
+                  </div>
+                </div>
+              </div>
+              <div class="aa-ItemActions">
+                <button
+                  class="aa-ItemActionButton aa-DesktopOnly aa-ActiveOnly"
+                  type="button"
+                  title="Select"
+                >
+                  <svg
+                    viewBox="0 0 24 24"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                  >
+                    <path
+                      d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
           </a>`
+        },
+        noResults() {
+          return 'No results.'
         },
       },
     }
@@ -58,5 +111,7 @@ export default function Search() {
     })
   }, [])
 
-  return <div ref={autocompleteContainer}></div>
+  return (
+    <div className="custom-search-wrapper" ref={autocompleteContainer}></div>
+  )
 }
--- utils/algolia.ts
+++ utils/algolia.ts
@@ -11,3 +11,20 @@ export const algoliaIndex = (): SearchIndex => {
   )
   return client.initIndex('games')
 }
+
+/**
+ * search client
+ */
+export const searchClient = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string,
+  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY as string
+)
+
+/**
+ * has AlgoliaConfig
+ * @returns
+ */
+export const hasAlgoliaConfig: boolean =
+  !!(process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string) &&
+  !!(process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY as string)
+// !!(process.env.ALGOLIA_SEARCH_ADMIN_KEY as string)
