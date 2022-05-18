327084b3d65914b2da09184eb369d83579f7d222
_XiaoTian <istianlei@qq.com>
2022-05-18 16:31:02 +0800

    ci: utils file test

--- __tests__/utils/file.test.ts
+++ __tests__/utils/file.test.ts
@@ -1,8 +1,13 @@
-import { parseFilename } from '../../utils/file'
+import { parseFileExtension, parseFilename } from '../../utils/file'
 
 describe('Utils file', () => {
   it('parseFilename', () => {
     const filename = parseFilename('test.png')
-    expect(filename).toEqual('test')
+    expect(filename).toBe('test')
+  })
+
+  it('parseUrl', () => {
+    const fileExtension = parseFileExtension('test.png')
+    expect(fileExtension).toBe('.png')
   })
 })
