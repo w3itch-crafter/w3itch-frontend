872179f325a7a726160286dd53c1b9cd002c3002
minakokojima <lychees67@gmail.com>
2022-05-27 05:31:12 +0800

    Update next-i18next.config.js

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   i18n: {
     defaultLocale: 'en-US',
-    locales: ['en-US', 'zh-CN','ja-JP','ru-RU','kr-KR'],
+    locales: ['en-US', 'zh-CN', 'ja-JP', 'ru-RU', 'kr-KR'],
     localePath: path.resolve('./public/locales'),
   },
 }
