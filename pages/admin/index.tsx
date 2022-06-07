15b7e0e3eaca2ef31e9b88e0568465698344229b
_XiaoTian <istianlei@qq.com>
2022-06-08 06:56:59 +0800

    fix: send transaction, theme, no cache (#254)
    * feat: metamask sendTransaction
    
    * feat: add zh-CN game header
    
    * fix: fix minetest username
    
    * feat: move code
    
    * feat: modify button theme
    
    * fix: conflict
    
    * fix: conflict
    
    * feat: theme
    
    * fix: minetest cache no-store
    
    * fix: conflict
    
    * fix: send transaction

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,16 +1,22 @@
 import LoadingButton from '@mui/lab/LoadingButton'
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
 import Stack from '@mui/material/Stack'
 import { initializationAlgoliaGame } from 'api/server'
+import useMetamask from 'hooks/useMetamask'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { NextSeo } from 'next-seo'
 import { useSnackbar } from 'notistack'
 import { useCallback, useState } from 'react'
+import { useWallet } from 'use-wallet'
 
 const Admin = () => {
   const { enqueueSnackbar } = useSnackbar()
   const [algoliaInitializationLoading, setAlgoliaInitializationLoading] =
     useState<boolean>(false)
+  const wallet = useWallet()
+
+  useMetamask()
 
   // algolia initialization
   const algoliaInitialization = useCallback(async () => {
@@ -41,6 +47,27 @@ const Admin = () => {
           >
             initialization
           </LoadingButton>
+        </Stack>
+        <h3>Metamask</h3>
+        <div>{wallet.status}</div>
+        <Stack direction="row" spacing={2}>
+          <Button
+            variant="contained"
+            onClick={() => {
+              window.MINETEST_METAMASK.sendTransaction('bob', '0.0000123')
+            }}
+          >
+            sendTransaction
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => wallet.connect('injected')}
+          >
+            MetaMask Connect
+          </Button>
+          <Button variant="contained" onClick={() => wallet.reset()}>
+            MetaMask disconnect
+          </Button>
         </Stack>
       </Box>
     </>
