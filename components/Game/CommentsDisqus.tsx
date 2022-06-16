65d787f7271d7833980f67ec06fb01b3c70cfe9f
xiaotian <istianlei@qq.com>
2022-06-16 19:00:46 +0800

    fix: render markdown theme

--- components/Game/CommentsDisqus.tsx
+++ components/Game/CommentsDisqus.tsx
@@ -1,9 +1,9 @@
 import Box from '@mui/material/Box'
 import CircularProgress from '@mui/material/CircularProgress'
 import { DiscussionEmbed } from 'disqus-react'
+import { useThemeReRender } from 'hooks'
 import { useRouter } from 'next/router'
-import { useTheme } from 'next-themes'
-import { FC, useEffect, useState } from 'react'
+import { FC } from 'react'
 
 interface Props {
   readonly title: string
@@ -11,14 +11,7 @@ interface Props {
 
 const CommentsDisqus: FC<Props> = ({ title }) => {
   const router = useRouter()
-  const [visible, setVisible] = useState(false)
-  const { resolvedTheme } = useTheme()
-  useEffect(() => {
-    setVisible(false)
-    setTimeout(() => {
-      setVisible(true)
-    }, 300)
-  }, [resolvedTheme])
+  const { visible } = useThemeReRender()
 
   return (
     <>
