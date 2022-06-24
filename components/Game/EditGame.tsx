567a3656789ce3db33a6d6656af2c54c42b4656d
Syntacticlosure <1227847322@qq.com>
2022-06-24 02:26:31 -0700

    feat: add In-game edit button when you are the owner of the game (#331)

---
+++ components/Game/EditGame.tsx
@@ -0,0 +1,21 @@
+import Link from '@mui/material/Link'
+import { AuthenticationContext } from 'context'
+import { FC, useContext } from 'react'
+import { GameEntity } from 'types'
+
+interface EditGameProps {
+  gameProject: GameEntity
+}
+
+const EditGame: FC<EditGameProps> = ({
+  gameProject: { id, username: projectUsername },
+}) => {
+  const {
+    state: { user },
+  } = useContext(AuthenticationContext)
+  const editRef = `/game/edit/${id}`
+  return user?.username === projectUsername ? (
+    <Link href={editRef}>Edit</Link>
+  ) : null
+}
+export default EditGame
