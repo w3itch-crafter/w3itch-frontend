886309b4905cdd1df58a2d5c9bde5d0c7a21d24a
xiaodao <lychees67@gmail.com>
2022-07-01 03:42:52 +0800

    Clean

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './account'
-export * from './blockchains'
-export * from './games'
-export * from './ratings'
-export * from './server'
-export * from './storage'
-export * from './users'
--- jest-shim.js
+++
@@ -1,5 +0,0 @@
-// https://stackoverflow.com/questions/57712235/referenceerror-textencoder-is-not-defined-when-running-react-scripts-test
-import { TextDecoder, TextEncoder } from 'util'
-
-global.TextEncoder = TextEncoder
-global.TextDecoder = TextDecoder
